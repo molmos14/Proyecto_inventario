f2b8846b3e74f5977bef041741f080e883e41866
molmos14 <molmosantillon14@gmail.com>
2022-10-21 11:05:51 -0500

    Update

--- Situación problema 1.py
+++ Situación problema 1.py
@@ -16,7 +16,7 @@ def main():
             elif num == 4:
                 datos_ventas(df) 
             elif num == 5:
-                ventas_vendedor(df)
+                actualizar_articulos_vendedor(df)
 
         else:
             print('\nSalir\n')
@@ -28,7 +28,7 @@ def Menu():
     2) Registrar llegada de artículos al almacén
     3) Consultar datos del inventario
     4) Consultar datos de las ventas
-    5) Mostrar reportes de ventas por vendedor o por artículo
+    5) Actualizar artículos del vendedor
     6) salir
     ''')
     num = int(input('Elija la opción que desee consultar: '))
@@ -99,6 +99,41 @@ def articulos_nuevos(df):
         }, ignore_index=True)
     df.to_csv('/home/m0lm0s/Descargas/inventario1.csv', index=None)
     return df
+
+def datos_inventario(df):
+    print(df)
+
+def datos_ventas(df):
+     print(f'\n{df.iloc[:,1:5]}')
+
+def actualizar_articulos_vendedor(df):
+    suma1 = 0
+    suma2 = 0
+    suma3 = 0
+    suma4 = 0
+    print('\nEste es el dataframe de los vendedores anterior')
+    print(f'\n{df.iloc[:4,6:]}')
+    for i in range(0,5):
+        suma1 += df.iat[i,4]
+    df.iat[0,7] += suma1
+
+    for i in range(5,10):
+        suma2 += df.iat[i,4]
+    df.iat[1,7] += suma2
+
+    for i in range(10,15):
+        suma3 += df.iat[i,4]
+    df.iat[2,7] += suma3
+
+    for i in range(15,20):
+        suma4 += df.iat[i,4]
+    df.iat[3,7] += suma4
+
+    print('\nEste es el dataframe de los vendedores actualizado')
+    print(f'\n{df.iloc[:4,6:]}')
+    df.to_csv('/home/m0lm0s/Descargas/inventario1.csv', index=None)
+
+
 
 if __name__ == '__main__':
     main()
